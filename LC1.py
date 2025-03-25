697b968105bce7f266d677e26ebc3500cfbf5593
rachit28042002 <rachitsanghvi28@gmail.com>
2025-03-25 14:24:07 +0530

    Initial commit

--- LC1.py
+++ LC1.py
@@ -28,7 +28,7 @@ Use the pieces of information provided in the context to answer user's question.
 If you dont know the answer, just say that you dont know, dont try to make up an answer. 
 Dont provide anything out of the given context
 
-Context: {context}
+Context: {context }
 Question: {question}
 
 Start the answer directly. No small talk please.
